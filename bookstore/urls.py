b3ba5683af935e86093319d60df62df4c86bd710
Pham Van Chung <pham.van.chung@sun-asterisk.com>
2021-05-09 23:44:41 +0700

    UserRegistration

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
+
     path("", include("pages.urls")),
 ]
